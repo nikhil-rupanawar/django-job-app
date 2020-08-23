90ece9f9fc164ba91fc4435be047ea50a9568c93
Nikhil Rupanawar <conikhil@gmail.com>
2020-08-23 19:46:20 +0530

    refactor code

--- jobapp/jobapp/jobapp/models.py
+++ jobapp/jobapp/jobapp/models.py
@@ -31,12 +31,12 @@ class JobStatus(enum.IntEnum):
 
 class UiStatus(models.TextChoices):
     PENDING = 'Pending'
-    REQUEST_ACK = 'Request acknowledged'
+    REQUEST_ACK = 'Acknowledged'
     RUNNING = 'Running'
     FAILED = 'Failed'
     ERRORED = 'Errored'
     SUCCESS = 'Success'
-    SUCCESS_WITH_WARNING = 'Success with warning'
+    SUCCESS_WITH_WARNING = 'Success with warning(s)'
 
 
 class Severity(enum.IntEnum):
@@ -49,22 +49,30 @@ class Severity(enum.IntEnum):
 
 
 ALL_STATUSES = tuple(JobStatus)
-FINAL_STATUSES = (JobStatus.FAILED, JobStatus.ERRORED, JobStatus.SUCCESS, JobStatus.SUCCESS_WITH_WARNING)
-SUCCESS_STATUSES = (JobStatus.SUCCESS, JobStatus.SUCCESS_WITH_WARNING)
-FAILED_STATUSES = (JobStatus.FAILED, JobStatus.ERRORED)
+FINAL_STATUSES = (
+    JobStatus.FAILED,
+    JobStatus.ERRORED,
+    JobStatus.SUCCESS,
+    JobStatus.SUCCESS_WITH_WARNING
+)
+GOOD_STATUSES = (JobStatus.SUCCESS, JobStatus.SUCCESS_WITH_WARNING)
+BAD_STATUSES = (JobStatus.FAILED, JobStatus.ERRORED)
 
 
-class Diagnostic(models.Model):
+class JobFailedError(Exception):
+    pass
+
+
+class AbstractDiagnostic(models.Model):
     
     class Meta:
         abstract = True
 
     severity = models.IntegerField(default=Severity.INFO.value)
-    message = models.CharField(null=True, max_length=255)
     details = postgres_fields.JSONField(null=True)
 
 
-class Job(models.Model):
+class AbstractJob(models.Model):
 
     DEFAULT_TTL_THRESHOLD = (3 * 24 * 60 * 60)
 
@@ -73,11 +81,10 @@ class Job(models.Model):
 
     _status = models.IntegerField(null=True)
     _ui_status = models.CharField(choices=UiStatus.choices, max_length=255)
+    _data = postgres_fields.JSONField(null=True)
     type = models.IntegerField(null=True)
     created_by = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    data = postgres_fields.JSONField(null=True)
-    _percentage_progress = models.IntegerField(null=True)
     created_at =  models.DateTimeField(auto_now_add=True)
     updated_at =  models.DateTimeField(auto_now_add=True)
     ttl = models.IntegerField(default=DEFAULT_TTL_THRESHOLD) # 3 days
@@ -89,15 +96,27 @@ class Job(models.Model):
     def ui_status(self):
         return self._ui_status
 
-    @property
-    def percentage_progress(self):
-        return self._percentage_progress
+    def acknowledge(self, save=True):
+        self.update_status(status=JobStatus.REQUEST_ACK, save=save)
 
-    @percentage_progress.setter
-    def percentage_progress(self, value):
-        self._percentage_progress = value
+    def running(self, save=True):
+        self.update_status(status=JobStatus.RUNNING, save=save)
 
-    def update_status(self, status: JobStatus=None, ui_status: UiStatus=None):
+    def fail(self, save=True, raise_error=True, reason=''):
+        self.update_status(status=JobStatus.FAILED, save=save)
+        if raise_error:
+            raise JobFailedError(f'Job failed, reason={reason}')
+
+    def success(self, save=True):
+        self.update_status(status=JobStatus.SUCCESS, save=save)
+
+    def error(self, save=True):
+        self.update_status(status=JobStatus.ERRORED, save=save)
+
+    def success_with_warning(self, save=True):
+        self.update_status(status=JobStatus.SUCCESS_WITH_WARNING, save=save)
+
+    def update_status(self, status: JobStatus, ui_status: UiStatus=None, save=True):
         assert status or ui_stauts
         self._status = status.value
         if ui_status is not None:
@@ -105,8 +124,15 @@ class Job(models.Model):
         else:
             mapped_status = getattr(UiStatus, status.name, None)
             if mapped_status is not None:
-                self._ui_status = mapped_status.value
+                self.update_ui_status(mapped_status, save=False)
         self.ping()
+        if save:
+            sellf.save()
+
+    def update_ui_status(self, status: UiStatus, save=True):
+        self._ui_status = ui_status
+        if save:
+            self.save()
 
     def ping(self):
         self.updated_at = now()
@@ -127,7 +153,7 @@ class Job(models.Model):
     def act(self):
         pass
 
-    def resume(self):
+    def act_resume(self):
         pass
 
     @property
@@ -142,23 +168,57 @@ class Job(models.Model):
         pass
 
 
-class DefaultJobRunner:
+class JobProgressMixin(models.Model):
+    class Meta:
+        abstract = True
+    total_units = models.IntegerField(db_column='progress_total_units')
+    done_units = models.IntegerField(default=0, db_column='progress_done_units')
+    progress_unit = models.CharField(max_length=50)
+    progress_unit_plural = models.CharField(max_length=50)
+    _progress_percent = models.IntegerField(null=True)
 
+    def report_progress(self, units:int=1, save=True):
+        self.done_units += units
+        self.remaining_units
+        if save:
+            self.save()
+
+    @property
+    def remaaining_units(self):
+        return self.total_units - self.done_units
+
+    @property
+    def progress_percent(self):
+        if self._progress_percent is not None:
+            return _progress_percent
+        return ((self.done_units * 100) / self.total_units)
+ 
+    @progress_percent.setter
+    def progress_percent(self, value: int):
+        assert 0 <= value <= 100
+        self._progress_percent = value
+
+
+class JobRunnerMixin:
     @classmethod
-    def run(cls, job: Type[Job]):
-        job.update_status(status=JobStatus.REQUEST_ACK)
-        job.publish_state()
+    def run(cls, job: Type[AbstractJob]):
+        job.acknowledge()
         try:
+            job.running()
             job.act()
+        except JobFailedError as e:
+            pass
         except Exception as e:
             logger.exception(e)
-            job.update_status(status=JobStatus.ERRORED)
-            job.publish_state()
+            job.error()
+        else:
+            if job.status not in FINAL_STATUSES:
+                job.success()
         finally:
             cls.process_post_job_hooks(job)
 
     @classmethod
-    def process_post_job_hooks(cls, job: Type[Job]):
+    def process_post_job_hooks(cls, job: Type[AbstractJob]):
         try:
             if job.status in SUCCESS_STATUSES:
                 job.on_success()
@@ -166,6 +226,7 @@ class DefaultJobRunner:
                 job.on_failure()
         except Exception as e:
             logger.exception(e)
+        finally:
             self.finalize()
 
 
@@ -195,66 +256,74 @@ class Groupset(models.Model):
     groups = models.ManyToManyField(Group)
 
 
-MOCK_DATA = {
-    'to_add_users': [1, 2, 3, 5, 6, 8],
-    'to_remove_users': [9, 4]
-}
-
-class GroupsetIdpSyncJob(Job):
-    groupset = models.ForeignKey(Groupset, on_delete=models.CASCADE) # demo only null true
+class GroupsetSyncJob(AbstractJob, JobProgressMixin, JobRunnerMixin):
+    # TODO: Do we need FK? IMO good to have for integrity
+    # groupset = models.ForeignKey(Groupset, on_delete=models.CASCADE)
 
     @classmethod
-    def sync_with_okta(cls, user):
-        time.sleep(1)
-        print(f'synced effective roles {user.effective_roles}'}
-
-    def add_user(self, user):
-        # Manage trasaction: TODO
+    def _add_user_to_groupset(self, user, groupset):
         with transaction.atomic():
             try:
-                self.groupset.users.add(user)
-                self.groupset.save()
-                self.sync_with_okta(user)
+                groupset.users.add(user)
+                groupset.save()
+                user.save()
+                user.sync_with_okta()
             except Exception as e:
                 logger.exception(e)
                 print(e)
+   
+    @property
+    def data(self):
+        # TODO real data
+        return {
+           'groupset_id': 1,
+           'to_add_users': [1, 2, 3, 5, 6, 8],
+           'to_remove_users': [9, 4]
+        }
 
     def act(self):
-        self.update_status(status=JobStatus.RUNNING)
-        self.publish_state()
-        to_add_users = User.objects.filter(id__in=MOCK_DATA['to_add_users'])
-        TOTAL_USERS = len(to_add_users)
-        PROCESSED_USERS = 0
-        self.percentage_progress = 0
-        self.publish_state()
+        groupset = Groupset.objects.get(id=self.data['groupset_id'])
+        to_add_users = User.objects.filter(id__in=self.data['to_add_users'])
         for user in to_add_users:
-            PROCESSED_USERS += 1
-            self.percentage_progress = int((PROCESSED_USERS * 100) / TOTAL_USERS)
-            self.publish_state()
-            print(f'processed user {user.username}')
-            self.groupsetidpsyncjobdiagnostic_set.create(
-                userid=user_id,
+            self._add_user_to_groupset(groupset, user)
+            print(f'Processed user {user.username}')
+            self.report_progress(units=1)
+            # handle exception and add dignostics
+            self.add_user_diagnostic(
+                user=user,
+                groupset=groupset,
                 job=self,
-                message=f'user processed successfully'
+                message=f'User {user.username} added successfully.',
+                serverity=Severity.INFO
             )
-        self.update_status(status=JobStatus.SUCCESS)
-        self.percentage_progress = 100
-        self.publish_state()
+        # TODO: remove user from groupset
         print(f'job completed')
 
-    def finalize(self):
-        pass
+    def add_user_diagnostic(
+        self,
+        user,
+        groupset,
+        messagge=None,
+        operation=None,
+        severity=Severity.INFO
+    ):
+        details = {
+           'groupset_id': groupset.id,
+           'user_id': user.id,
+           'operation': operation,
+           'message': message,
+        }
+        self.groupsetsyncjobdiagnostic_set.create(
+            job=self,
+            details=details,
+            severity=severity
+        )
 
 
-class GroupsetIdpSyncJobDiagnostic(Diagnostic):
-    job = models.ForeignKey(GroupsetIdpSyncJob,  on_delete=models.CASCADE)
-    userid = models.IntegerField(null=True)
-    operation = models.CharField(max_length=10, choices=['add', 'remove'])
+class GroupsetSyncJobDiagnostic(AbstractDiagnostic):
+    job = models.ForeignKey(GroupsetSyncJob,  on_delete=models.CASCADE)
 
-class DeleteGroupsetJob(GroupsetIdpSyncJob):
+
+class DeleteGroupsetJob(GroupsetSyncJob):
     class Meta:
         proxy = True
-
-    def act(self):
-        super().act()
-
